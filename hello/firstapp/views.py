18965f818f75b4778037db14fca74d4900cc8477
Your Name <you@example.com>
2024-09-12 11:56:28 +0300

    12.09.24 ЛЗ 5. Турышев, Радышевский. Реализовали возможность переадресации и отправки пользователю статусных кодов.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,11 +1,28 @@
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+ return HttpResponse("<h2>Главная</h2>")
+def about(request):
+ return HttpResponse("<h2>О сайте</h2>")
+def contact(request):
+ return HttpResponse("<h2>Контакты</h2>")
+def products(request, productid):
+ category = request.GET.get("cat", "")
+ output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
+ return HttpResponse(output)
+def users(request):
+ id = request.GET.get("id", 1)
+ name = request.GET.get("name", "Максим")
+ output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3 >" .format(id, name)
+ return HttpResponse(output)
 
 def index(request):
-    return HttpResponse("<h2>Глaвнaя</h2>")
+ return HttpResponse("Index")
 def about(request):
-    return HttpResponse("<h2>0 сайте</h2>")
+ return HttpResponse("About")
 def contact(request):
-    return HttpResponse("<h2>Koнтaкты</h2>")
+ return HttpResponseRedirect("/about")
+def details(request):
+ return HttpResponsePermanentRedirect("/")
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,14 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import re_path
+from django.urls import path, re_path
 from firstapp import views
 
 urlpatterns = [
-re_path (r'^contact/', views.contact),
-re_path(r'^about', views.about),
-path ('', views. index),
+ path('', views.index),
+ re_path(r'^about', views.about),
+ re_path(r'^contact', views.contact),
+ path('products/<int:productid>/', views.products),
+ path('users/', views.users),
 ]
+
+urlpatterns = [
+ path('', views.index),
+ path('about/', views.about),
+ path('contact/', views.contact),
+ path('details/', views.details),
+]
+
+
 
 
