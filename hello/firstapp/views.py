7bcea9c9659f890a3c680bf91927268d7fe21133
Patrikk87 <Patrikstar16724@gmail.com>
2024-09-15 22:50:06 +0300

    15.09.24. ПЗ-2 (прошлый коммит "ПЗ-4" относится к лекции №4  (опечатка)) Турышев, Радышевский. Выполнили практическое занятие и научились пользоваться параметрами представлений.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,28 +1,22 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
- return HttpResponse("<h2>Главная</h2>")
-def about(request):
- return HttpResponse("<h2>О сайте</h2>")
-def contact(request):
- return HttpResponse("<h2>Контакты</h2>")
-def products(request, productid):
- category = request.GET.get("cat", "")
- output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
- return HttpResponse(output)
-def users(request):
- id = request.GET.get("id", 1)
- name = request.GET.get("name", "Максим")
- output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3 >" .format(id, name)
- return HttpResponse(output)
+
 
 def index(request):
- return HttpResponse("Index")
+    return HttpResponse("<h2>Главная</h2>")
+    
 def about(request):
- return HttpResponse("About")
+    return HttpResponse("<h2>О сайте</h2>")
+
 def contact(request):
- return HttpResponseRedirect("/about")
-def details(request):
- return HttpResponsePermanentRedirect("/")
+    return HttpResponse("<h2>Контакты</h2>")
+
+def products(request, productid=1):
+    output = "<h2>Продукт № {0}</h2>".format(productid)
+    return HttpResponse(output)
+
+def users(request, id=1, name='Максим'):
+    output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3>".format(id, name)
+    return HttpResponse(output)
+
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,23 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
+from django.urls import re_path
 from firstapp import views
 
 urlpatterns = [
- path('', views.index),
- re_path(r'^about', views.about),
- re_path(r'^contact', views.contact),
- path('products/<int:productid>/', views.products),
- path('users/', views.users),
+    path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    path('products/', views.products), # маршрут по умолчанию
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users), # маршрут по умолчанию
+    path('users/<int:id>/<str:name>/', views.users),
 ]
 
-urlpatterns = [
- path('', views.index),
- path('about/', views.about),
- path('contact/', views.contact),
- path('details/', views.details),
-]
+
+
 
 
 
