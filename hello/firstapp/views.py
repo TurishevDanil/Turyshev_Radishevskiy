17f00d21a23dc8172f8d58659f343fb73e0cb534
Patrikk87 <Patrikstar16724@gmail.com>
2024-09-17 22:39:37 +0300

    17.09.24. ПЗ-4. Радышевский. Научился определять формы и реализовал простейший вариант формы, отправляющей пользователю введенные им данные по кнопке.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import *
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.template.response import TemplateResponse
+from .forms import UserForm
 
-def index(request):
-    return HttpResponse("<h2>Главная</h2>")
-
-def about(request):
-    return HttpResponse("<h2>О сайте</h2>")
-
-def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
 
 def products(request, productid):
-     category = request.GET.get("cat", "")
-     output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
-     return HttpResponse(output)
+    category = request.GET.get("cat", "")
+    output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
+    return HttpResponse(output)
 
 def users(request):
     id = request.GET.get("id", 1)
@@ -23,8 +17,6 @@ def users(request):
     output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3 >" .format(id, name)
     return HttpResponse(output)
 
-def index(request):
-    return HttpResponse("Index")
 
 def about(request):
     return HttpResponse("About")
@@ -33,11 +25,17 @@ def contact(request):
     return HttpResponseRedirect("/about")
 
 def details(request):
-     return HttpResponsePermanentRedirect("/")
+    return HttpResponsePermanentRedirect("/")
 
 def index(request):
- cat = ["Ноутбуки", "Принтеры", "Сканеры", "Диски", "Шнуры"]
- return render(request, "firstapp/index.html", context={"cat": cat})
+    if request.method == "POST":
+        name = request.POST.get("name") # получить значения поля Имя
+        age = request.POST.get("age") # значения поля Возраст
+        output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
+        return HttpResponse(output)
+    else:
+        userform = UserForm()
+        return render(request, "index.html", {"form": userform})
 
 
 
