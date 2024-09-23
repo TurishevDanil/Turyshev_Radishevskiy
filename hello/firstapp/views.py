8eef361ee14f13fda9924424e399b3b13ed4dbd7
Patrikk87 <Patrikstar16724@gmail.com>
2024-09-23 22:13:41 +0300

    23.09.24. ЛЗ-9. Радышевский. Приобрел общее понятие о работе с полями форм, практиковался с их созданием и стилизацией.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -29,7 +29,13 @@ def details(request):
 
 def index(request):
     userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data["name"]
+            return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
     return render(request, "index.html", {"form": userform})
+
 
 
 
