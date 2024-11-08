f33184adf04532f7848593102707fc5b593eebc5
Your Name <you@example.com>
2024-11-08 09:38:36 +0300

    08.11.2024. ПЗ_14. Турышев. Научился использовать поля для ввода данных. Поля: FileField, FilePathField, FloatField, GenericIPAddressField, lmageField, IntegerField, JsonField, MultipleChoiceField, NullBooleanField, RegexField, SlugField, TimeField, TypedChoiceField, TypedMultipleChoiceField, URLField, UUIDField.

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -1,43 +1,65 @@
 from django import forms
 
 # class UserForm(forms.Form):
-#  name = forms.CharField(label="Имя клиента")
-#  age = forms.IntegerField(label="Возраст клиента")
-
+#  file = forms.FileField(label="Файл")
 
 # class UserForm(forms.Form):
-#    basket = forms.BooleanField(label="Положить товар в корзину", required=False)
+#  file_path = forms.FilePathField(label="Выберите файл",
+#  path="C:/my_doc/")
 
 # class UserForm(forms.Form):
-#  name = forms.CharField(label="Имя клиента")
+#  file_path = forms.FilePathField(label="Выберите файл",
+#  path="C:/my_doc/",
+#  allow_files="True",
+#  allow_folders="True")
 
 # class UserForm(forms.Form):
-#  name = forms.CharField(label="Имя клиента", max_length=15,
-#  help_text="ФИО не более 15 символов")
+#  num = forms.FloatField(label="Введите число")
 
 # class UserForm(forms.Form):
-#  ling = forms.ChoiceField(label="Выберите язык",
-#  choices=(
-#  (1, "Английский"),
-#  (2, "Немецкий"),
-#  (3, "Французский")))
+#  ip_adres = forms.GenericIPAddressField(label="IP адрес",
+#  help_text=" Пример формата 192.0.2.0")
 
 # class UserForm(forms.Form):
-#  date = forms.DateField(label="Введите дату")
+#  file = forms.ImageField(label="Изображение")
 
 # class UserForm(forms.Form):
-#  date_time = forms.DateTimeField(label="Введите дату и время")
+#  num = forms.IntegerField(label="Введите целое число")
 
 # class UserForm(forms.Form):
-#  num = forms.DecimalField(label="Введите десятичное число")
+#  num = forms.JSONField(label="Данные формата JSON")
 
 # class UserForm(forms.Form):
-#  num = forms.DecimalField(label="Введите десятичное число",
-#  decimal_places=2)
+#  country = forms.MultipleChoiceField(label="Выберите страны",
+#  choices=((1, "Англия"),
+#           (2, "Германия"),
+#           (3, "Испания"),
+#           (4, "Россия")))
+ 
+# class UserForm(forms.Form):
+#  vyb = forms.NullBooleanField(label="Вы поедете в Сочи этим летом?")
 
 # class UserForm(forms.Form):
-#  time_delta = forms.DurationField(label="Введите промежуток времени")
+#  reg_text = forms.RegexField(label="Текст", regex="^[0-9][A-F][0-9]$")
+
+# class UserForm(forms.Form):
+#  slug_text = forms.SlugField(label="Введите текст")
+
+# class UserForm(forms.Form):
+#  time = forms.DateField(label="Введите время")
+
+# class UserForm(forms.Form):
+#  city = forms.TypedMultipleChoiceField(label="Выберите город",
+#  empty_value=None,
+#  choices=((1, "Москва"),
+#           (2, "Воронеж"),
+#           (3, "Курск"),
+#           (4, "Томск")))
+
+# class UserForm(forms.Form):
+#  url_text = forms.URLField(label="Введите URL",
+#  help_text="Например http://www.yandex.ru")
 
 class UserForm(forms.Form):
- email = forms.EmailField(label="Электронный адрес",
- help_text="Обязательный символ - @")
+ uuid_text = forms.UUIDField(label="Введите UUID",
+ help_text="Формат xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
